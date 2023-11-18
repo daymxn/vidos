dd16778e601fd5c6b59b8007d1a39ef144b8271b
Daymon <daymxn@users.noreply.github.com>
2023-11-18 11:59:58 -0600

    Domain JSDocs

--- src/controllers/domain.ts
+++ src/controllers/domain.ts
@@ -1,11 +1,24 @@
-import chalk from "chalk";
 import { encodeClassToJSON } from "@src/util";
+import chalk from "chalk";
 
+/**
+ * Status representing if a domain is enabled or not.
+ *
+ * @enum {number}
+ */
 enum DomainStatus {
   INACTIVE = 0,
   ACTIVE = 1,
 }
 
+/**
+ * Common interface for creating and interacting with domains.
+ *
+ * @property {string} file_name - nginx config file name representation for this domain.
+ * @property {string} source - The http domain to direct traffic from.
+ * @property {string} destination - The local IP to direct traffic to.
+ * @property {DomainStatus} status - Whether the domain is currently enabled or not.
+ */
 class Domain {
   public readonly file_name: string;
 
@@ -19,16 +32,36 @@ class Domain {
     this.file_name = `${source}-${destWithFixedPorts}.conf`;
   }
 
+  /**
+   * Creates a Domain instance from a JSON string.
+   *
+   * @param {string} str - The JSON string to parse.
+   * @returns {Domain} - The created Domain instance.
+   */
   static fromString(str: string): Domain {
     const json = JSON.parse(str);
 
     return this.fromObject(json);
   }
 
+  /**
+   * Creates a Domain instance from a javascript object.
+   *
+   * @param {Object} obj - The javascript object with domain properties.
+   * @returns {Domain} - The created Domain instance.
+   */
   static fromObject(obj: { [key: string]: any }): Domain {
     return new Domain(obj.source, obj.destination, obj.status);
   }
 
+  /**
+   * Generates a pretty string representation of the domain.
+   *
+   * Intended to be used when displaying the domain the CLI; with pretty colors
+   * and formatting.
+   *
+   * @returns {string} - The pretty string representation.
+   */
   prettyString(): string {
     const str = `${chalk.green(this.source)} => ${chalk.blue(this.destination)}`;
 
@@ -36,10 +69,16 @@ class Domain {
     return str;
   }
 
+  /**
+   * Converts the Domain instance to a JSON string.
+   *
+   * Will filter out properties not intended to be saved.
+   *
+   * @returns {string} - The JSON string representation of the instance.
+   */
   toString(): string {
     return encodeClassToJSON(this, ["file_name"]);
   }
 }
 
-export { Domain };
-export { DomainStatus };
+export { Domain, DomainStatus };
